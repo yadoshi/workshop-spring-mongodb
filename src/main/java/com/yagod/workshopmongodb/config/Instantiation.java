3259cf81a20cd10422c252a2c4459f3b4218ecae
yadoshi <yagodfernandes@gmail.com>
2024-01-26 16:27:04 -0300

    projecao dos dados do autor com dto

--- src/main/java/com/yagod/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/yagod/workshopmongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.yagod.workshopmongodb.config;
 
 import com.yagod.workshopmongodb.domain.Post;
 import com.yagod.workshopmongodb.domain.User;
+import com.yagod.workshopmongodb.dto.AuthorDTO;
 import com.yagod.workshopmongodb.repositories.PostRepository;
 import com.yagod.workshopmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,8 +35,10 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+        repo.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         repo.saveAll(Arrays.asList(maria,alex,bob));
         postRepository.saveAll(Arrays.asList(post1,post2));

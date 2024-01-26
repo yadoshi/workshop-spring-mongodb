2e286d4057a593d5d9aba07c918a6af171ff78c2
yadoshi <yagodfernandes@gmail.com>
2024-01-26 14:08:10 -0300

    padrao dto em usuarios

--- src/main/java/com/yagod/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/yagod/workshopmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.yagod.workshopmongodb.resources;
 
 import com.yagod.workshopmongodb.domain.User;
+import com.yagod.workshopmongodb.dto.UserDTO;
 import com.yagod.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(listDto);
     }
 }

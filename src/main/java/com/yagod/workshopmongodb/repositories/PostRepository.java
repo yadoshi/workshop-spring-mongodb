204a2718391ffdd6481e479327e109e7478a068c
yadoshi <yagodfernandes@gmail.com>
2024-02-01 10:41:39 -0300

    consulta simples com @query

--- src/main/java/com/yagod/workshopmongodb/repositories/PostRepository.java
+++ src/main/java/com/yagod/workshopmongodb/repositories/PostRepository.java
@@ -3,12 +3,16 @@ package com.yagod.workshopmongodb.repositories;
 import com.yagod.workshopmongodb.domain.Post;
 import com.yagod.workshopmongodb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{'title': {$regex: ?0, $options: 'i'}}")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }

ff3727da53f662010e6be7918a126b90e6ff75fb
yadoshi <yagodfernandes@gmail.com>
2024-02-01 09:57:09 -0300

    implementando endpoint para retornar os posts de um usuario

--- src/main/java/com/yagod/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/yagod/workshopmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.yagod.workshopmongodb.resources;
 
+import com.yagod.workshopmongodb.domain.Post;
 import com.yagod.workshopmongodb.domain.User;
 import com.yagod.workshopmongodb.dto.UserDTO;
 import com.yagod.workshopmongodb.services.UserService;
@@ -55,4 +56,9 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
+    }
 }

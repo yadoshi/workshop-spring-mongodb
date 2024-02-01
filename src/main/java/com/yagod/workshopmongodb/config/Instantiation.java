b65e57af15bb716cd64daa77e0fcc809d91e8ef3
yadoshi <yagodfernandes@gmail.com>
2024-02-01 10:15:19 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/yagod/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/yagod/workshopmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.yagod.workshopmongodb.config;
 import com.yagod.workshopmongodb.domain.Post;
 import com.yagod.workshopmongodb.domain.User;
 import com.yagod.workshopmongodb.dto.AuthorDTO;
+import com.yagod.workshopmongodb.dto.CommentDTO;
 import com.yagod.workshopmongodb.repositories.PostRepository;
 import com.yagod.workshopmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         repo.saveAll(Arrays.asList(maria,alex,bob));
         postRepository.saveAll(Arrays.asList(post1,post2));
--- src/main/java/com/yagod/workshopmongodb/domain/Post.java
+++ src/main/java/com/yagod/workshopmongodb/domain/Post.java
@@ -1,10 +1,13 @@
 package com.yagod.workshopmongodb.domain;
 
 import com.yagod.workshopmongodb.dto.AuthorDTO;
+import com.yagod.workshopmongodb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -16,6 +19,8 @@ public class Post {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){
     }
@@ -66,6 +71,14 @@ public class Post {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override

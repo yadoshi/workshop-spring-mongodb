a167dd76001bbb92cdd5ee71459a8cfbe43bf733
yadoshi <yagodfernandes@gmail.com>
2024-02-01 10:36:19 -0300

    Consulta simples com querry methods

--- src/main/java/com/yagod/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/yagod/workshopmongodb/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.yagod.workshopmongodb.resources;
 import com.yagod.workshopmongodb.domain.Post;
 import com.yagod.workshopmongodb.domain.User;
 import com.yagod.workshopmongodb.dto.UserDTO;
+import com.yagod.workshopmongodb.resources.util.URL;
 import com.yagod.workshopmongodb.services.PostService;
 import com.yagod.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
